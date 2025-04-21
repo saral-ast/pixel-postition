ad020e9fc477e92cade37ccc811763fd682fc3ab
Saral-Chauhangit <saralchauhan424t@gmail.com>
2025-04-21 13:47:51 +0530

    add CORS configuration and update Vite server settings

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,18 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         tailwindcss(),
     ],
+    server: {
+        cors: {
+            origin: [
+                "http://localhost:5174",
+                "http://localhost:8000",
+                "http://localhost:3000",
+            ],
+        },
+    },
 });
